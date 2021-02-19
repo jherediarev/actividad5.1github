3a758d3befd06d55463ecf906753dedf57a76cdc
Johnny Reveles <Johnny Reveles@LAPTOP-PO4S021M>
2021-02-18 22:43:59 -0600

    Es mi primer commit

---
+++ src/git66659/Git66659.java
@@ -0,0 +1,26 @@
+/*
+ UNVERSIDAD AUTÓNOMA DE CAMPECHE
+ Facultad de Ingeniería
+ Ingeniería en Sistemas Computacionales
+ */
+package git66659;
+
+/**
+ *
+ * @author Johnny Reveles
+ */
+public class Git66659 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        String nombre = "JOHNNY ABJOMAR";
+        String matricula = "66659";
+        System.out.println("SOY " + nombre + " Y MI MATRICULA ES " + matricula);
+        
+        // ACABO DE CREAR MI REPOSITORIO LOCAL
+    }
+    
+}
